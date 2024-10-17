e59aafb058604a689452021abc1b790ac6b48e82
cprisbrey1 <cprisbrey1@mail.pima.edu>
2024-10-16 21:41:40 -0700

    Update CIS129_corgan_prisbrey_Lab5.py

--- CIS129_corgan_prisbrey_Lab5.py
+++ CIS129_corgan_prisbrey_Lab5.py
@@ -1,47 +1,28 @@
-#Module 4 Lab-4
-#Corgan Prisbrey
-#oct 8 2024
-#this caclualted the bonus's recieved based of increase in sales and total sales
+#corgan prisbrey CIS129 lab 5
 
+keep_going = 'y'
+payout_per_bottle = .1
 
-monthlySales = 0 #monthly sales amount
-storeAmount = 0 #store bonus amount
-empAmount = 0 # employe bonus amount
-salesIncrease = 0 # percent of sales increase
-prompt = "what is the monthly sale amount?"
+while keep_going == 'y':
+    total_bottles = 0
+    counter = 1
+    today_bottles = 0
+    total_payout = 0
 
-#this code determines monthly sales
-monthlySales = float(input(prompt))
+    while counter <= 7:
+        today_bottles = int(input(f"Enter number of bottles for day #{counter}:  "))
+        total_bottles += today_bottles
+        counter += 1
+   
+    total_payout = total_bottles * payout_per_bottle
+    
+    print()
 
-# this code determines the storeAmount bonus
+    print(f"The total number of bottles collected is {total_bottles}")
+    print(f"The total paid out is $ {total_payout}")
 
-if monthlySales >= 110000:
-    storeAmount = 6000
-elif monthlySales >= 100000:
-    storeAmount = 5000
-elif monthlySales >= 90000:
-    storeAmount = 4000
-elif monthlySales >= 80000:
-    storeAmount = 3000
-else:
-    storeAmount = 0
+    print()
 
-#this code gets the percent of increase in sales 
-salesIncrease = float(input("what is the sales increase"))
-salesIncrease = salesIncrease/100
-
-#this code determines the empAmount bonus
-if salesIncrease >= .05:
-    empAmount = 75
-elif salesIncrease >= .04:
-    empAmount = 50
-elif salesIncrease >= .03:
-    empAmount = 40
-else:
-    empAmount = 0
-
-# this code prints the bonus
-print(f"The store bonus amount is ${storeAmount}")
-print(f"The employee bonus amount is ${empAmount}")
-if storeAmount == 6000 and empAmount == 75:
-    print("Congrats! You have reached the highest bonus amounts possible!")
+    print("Do you want to enter another week's worth of data?")
+    keep_going = input("(Enter y or n): ")
+    print()
